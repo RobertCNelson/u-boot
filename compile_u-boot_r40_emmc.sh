924ad8fee7aff3e39423c6647284c23b8b4c1d62
juanesf <juanesf91@gmail.com>
2020-02-12 16:49:56 -0300

    update compile scripts

--- compile_u-boot_r40_emmc.sh
+++ compile_u-boot_r40_emmc.sh
@@ -8,7 +8,7 @@ cd /tmp
 echo "\033[36m cloning u-boot.\033[0m"
 git clone https://github.com/juanesf/u-boot.git && cd u-boot && make Bananapi_M2_Ultra_defconfig && make
 echo "\033[36m make boot.scr.\033[0m"
-mkimage -C none -A arm -T script -d /boot/boot.cmd boot.scr
+mkimage -C none -A arm -T script -d /tmp/u-boot/r40_boot.cmd boot.scr
 echo "\033[36m copying binary u-boot emmc.\033[0m"
 sudo dd if=/tmp/u-boot/u-boot-sunxi-with-spl.bin of=/dev/mmcblk0p0 bs=1024 seek=8
 echo "\033[36m removing dtb.\033[0m"
